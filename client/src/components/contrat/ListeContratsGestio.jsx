5c36c7661499126c283b88663efd6a038086d538
amine <dridiimedamine@gmail.com>
2024-09-20 18:24:24 +0200

    mise à jour data et filtre

--- client/src/components/contrat/ListeContratsGestio.jsx
+++ client/src/components/contrat/ListeContratsGestio.jsx
@@ -18,7 +18,7 @@ function ListeContratsGestio() {
   useEffect(() => {
     const fetchContrats = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/contrats');
+        const response = await fetch('http://51.83.69.195:5000/api/contrats');
         if (!response.ok) {
           throw new Error('Erreur lors de la récupération des contrats');
         }
@@ -51,7 +51,7 @@ function ListeContratsGestio() {
 
   const handleSaveClick = async (id) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/contrats/${id}`, {
+      const response = await fetch(`http://51.83.69.195:5000/api/contrats/${id}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
@@ -98,7 +98,7 @@ function ListeContratsGestio() {
 
   const handleDeleteClick = async (id) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/contrats/${id}`, {
+      const response = await fetch(`http://51.83.69.195:5000/api/contrats/${id}`, {
         method: 'DELETE',
       });
 
--- client/src/components/contrat/Souscription.jsx
+++ client/src/components/contrat/Souscription.jsx
@@ -37,7 +37,7 @@ function Souscription({ setIsAdding }) {
       try {
         const token = localStorage.getItem('authToken'); // Récupère le token d'authentification depuis le localStorage
         if (token) {
-          const response = await axios.get('http://localhost:5000/api/profile', {
+          const response = await axios.get('http://51.83.69.195:5000/api/profile', {
             headers: {
               Authorization: `Bearer ${token}`, // Envoie le token dans les en-têtes
             },
@@ -88,7 +88,7 @@ function Souscription({ setIsAdding }) {
     };
   
     try {
-      const response = await fetch('http://localhost:5000/api/contrats', {
+      const response = await fetch('http://51.83.69.195:5000/api/contrats', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
