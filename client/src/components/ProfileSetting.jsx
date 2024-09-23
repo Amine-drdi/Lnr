e1b33d32713e3c493a106631eb898cd900b3fcab
amine <dridiimedamine@gmail.com>
2024-09-23 16:54:55 +0200

    new update date signature

--- client/src/components/ProfileSetting.jsx
+++ client/src/components/ProfileSetting.jsx
@@ -15,7 +15,7 @@ function ProfileSetting() {
     const fetchUser = async () => {
       try {
         const token = localStorage.getItem('authToken');
-        const response = await axios.get('http://localhost:5000/api/profile', {
+        const response = await axios.get('http://51.83.69.195:5000/api/profile', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
