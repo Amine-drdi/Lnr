f66cc0539805fcfb879bfcb033fdd4630ebf7f75
amine <dridiimedamine@gmail.com>
2024-10-01 13:23:47 +0200

    modification

--- client/src/components/contrat/Souscription.jsx
+++ client/src/components/contrat/Souscription.jsx
@@ -302,8 +302,8 @@ function Souscription({ setIsAdding }) {
           <option value="Cyrine Ben Aicha">Cyrine Ben Aicha</option>
           <option value="Sihem Selemi">Sihem Selemi</option>
           <option value="Hajer Askri">Hajer Askri</option>
-          <option value="Eya Ben Jabra">Eya Ben Jabra</option>
           <option value="Rim Dabebi ">Rim Dabebi </option>
+          <option value="Eya Ben Jabra">Eya Ben Jabra</option>
           <option value="Leads">Leads </option>
          
         </select>
