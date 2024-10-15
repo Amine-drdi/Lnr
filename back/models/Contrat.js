e877f9da50e0fbdfd8e1ad818e864259b403b12b
amine <dridiimedamine@gmail.com>
2024-10-15 18:23:44 +0200

    allez

--- back/models/Contrat.js
+++ back/models/Contrat.js
@@ -5,32 +5,7 @@ const mongoose = require('mongoose');
 const contratSchema = new mongoose.Schema({
 
 
- /* nom: { type: String, required: true },
-  prenom: { type: String, required: true },
-  telephone: { type: String, required: true },
-  email: { type: String, required: true },
-  dob: { type: String, required: true },
-  address: { type: String, required: true },
-  profession: { type: String, required: true },
-  signatureDate: { type: String , required: true },
-  cotisation: { type: String, required: true },
-  compagnie: { type: String, required: true },
-  effetDate: { type: String, required: true },
-  fraisEntre: { type: String  },
-  fraisDossier: { type: String },
-  apporteurAffaire: {type: String , required: true},
-  interetClient: { type: String },
-  etatDossier: { type: String },
-  commentaire: { type: String},
-  Commercial :{ type: String, },
-  ancienneMutuelle: {type: String},
-  remarque: {type : String},
-  retourCompagnie : {type : String},
-  suivieGestion : { type : String} ,
-  typeResiliation : { type : String} ,
-  file: {type : String} ,
-  status: { type: String, default: 'pending' }, // Le statut du contrat
-  createdAt: { type: Date, default: Date.now },*/
+
   nom: String,
   prenom: String,
   telephone: String,
