a7d2a1c9da37eb813a7286ea283ee6d28b051b53
amine <dridiimedamine@gmail.com>
2024-10-30 12:33:47 +0100

    numOPCO

--- client/src/components/Opco/ListeRdvManager.jsx
+++ client/src/components/Opco/ListeRdvManager.jsx
@@ -140,7 +140,7 @@ function ListeRdvManager() {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200">
-            {filteredRdvs.map((RDV) => (
+            {filteredRdvs.map((RDV , index) => (
               <tr key={RDV._id} className="hover:bg-gray-50 transition-colors">
               <td className="px-4 py-3 text-sm text-gray-700 text-center">{index + 1}</td>
                 <td className="px-4 py-3 text-sm text-gray-700">
